5cff7efcaa99bf7b226e849d267a199eda0ff7be
christian <xtiangarcia02444@gmail.com>
2024-04-29 13:09:55 +0800

    Eas

--- rn-dog-feeder-app/App.js
+++ rn-dog-feeder-app/App.js
@@ -4,11 +4,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, createNavigationContainerRef } from "@react-navigation/native";
 import { FONTS } from "./constants/fonts";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect} from "react";
 import { Login, SignUp, Welcome, Home, DeviceScreen, Account} from "./screens";
 import { StyleSheet } from "react-native";
 import { doc, getDoc } from "firebase/firestore";
 import { initializeApp } from '@firebase/app';
+
 import { auth } from "./auth/config";
 import { onAuthStateChanged } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
@@ -18,16 +19,9 @@ import { Device } from "./screens/home";
 
 const Stack = createNativeStackNavigator();
 
-
-
-
-
-
-
 export default function App() {
   const [fonstLoaded] = useFonts(FONTS);
   const [currentArray, setCurrentArray] = useState(null);
-  console.log(currentArray)
   const onLayoutRootView = useCallback(async () => {
     if (fonstLoaded) {
       await SplashScreen.hideAsync();
@@ -37,6 +31,9 @@ export default function App() {
   if (!fonstLoaded) {
     return null;
   }
+  
+
+
   return (
     <SafeAreaProvider onLayout={onLayoutRootView}>
       <NavigationContainer>
@@ -51,7 +48,7 @@ export default function App() {
            {props => <Home {...props} currentArray={currentArray} setCurrentArray={setCurrentArray} />}
           </Stack.Screen>
           {currentArray?.slice(1).map(device=> {
-            return <Stack.Screen key = {device} name={device} component={DeviceScreen} initialParams={{device}} />
+            return <Stack.Screen key = {device} name={device} component={DeviceScreen} initialParams={{device}}  />
           })}
         </Stack.Navigator>
       </NavigationContainer>
--- rn-dog-feeder-app/screens/deviceScreen.js
+++ rn-dog-feeder-app/screens/deviceScreen.js
@@ -1,16 +1,28 @@
-import { StyleSheet, Text, View, TouchableHighlight, TouchableWithoutFeedback, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight, TouchableWithoutFeedback, Image, TouchableOpacity, Modal } from 'react-native'
 import { getFirestore, collection, getDocs, getDoc, doc, onSnapshot, updateDoc, addDoc, setDoc, FieldValue} from 'firebase/firestore';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react'
 import { db } from './home';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DateTimePicker from '@react-native-community/datetimepicker'
+// import firestore from '@react-native-firebase/firestore';
+
+// // Enable offline persistence
+// firestore().settings({
+//   persistence: true,
+// });
 
 const DeviceScreen = ({route}) => {
+    const [dateModalOpen, setDateModalOpen] = useState(false)
+    const [feedTimes, setFeedTimes] = useState([]);
+    const [motorState, setMotorState] = useState(null);
+
+    
+    
     const navigator = useNavigation();
     const {device} = route.params;
-    const [motorState, setMotorState] = useState(null)
     const deviceRef = doc(db, "device-feeder", device);
     (async function () {
         const initialMotorState = await getDoc(deviceRef);
@@ -44,15 +56,113 @@ const DeviceScreen = ({route}) => {
         </View>
 
         <View style={{justifyContent:"center", alignItems: "center",  height: 300, width: "100%"}}>
-        <TouchableOpacity style={{width: "90%", backgroundColor: "#000", borderWidth: 2, padding: 10, alignItems: "center"}} onPress={updateMotorState}>
-          <Text style={{color:"#fff", textTransform:"uppercase", fontWeight: "bold", fontSize: 18, padding: 5}}>Feed</Text>
+        <TouchableOpacity style={{marginBottom: 10, width: "90%", backgroundColor: "#000", borderWidth: 2, padding: 10, alignItems: "center"}} onPress={updateMotorState}>
+          <Text style={{color:"#fff", textTransform:"uppercase", fontWeight: "bold", fontSize: 18, padding: 5}}>Feed now</Text>
         </TouchableOpacity> 
 
-          
+
+        <TouchableOpacity style={{width: "90%", backgroundColor: "#FFF", borderWidth: 2, padding: 10, alignItems: "center"}} onPress={()=> setDateModalOpen(true)}>
+          <Text style={{color:"#000", textTransform:"uppercase", fontWeight: "bold", fontSize: 18, padding: 5}}>Set Feeding Time</Text>
+        </TouchableOpacity> 
+          <CalendarModal dateModalOpen={dateModalOpen} setDateModalOpen={setDateModalOpen} updateMotorState={updateMotorState} deviceRef={deviceRef}/>
         </View>
         
     </SafeAreaView>
    
+}
+
+
+function CalendarModal({dateModalOpen, setDateModalOpen, updateMotorState, deviceRef}){
+    const [showDatePicker, setShowDatePicker]  = useState(false)
+    const [showTimePicker, setShowTimePicker] = useState(date)
+    const [date, setDate] = useState(new Date());
+    const [time, setTime] = useState(date)
+    const [feedTimes, setFeedTimes] = useState(null)
+    useEffect(()=>{
+        getDoc(deviceRef).then(res=> {
+            if(res.data().feedTimes.length < 1) return;
+            setFeedTimes(res.data().feedTimes);
+        })
+    
+    },[])
+
+    useEffect(()=>{
+        feedTimes?.forEach(time=> {
+            const delay = time - Date.now();
+            if(delay<0) return;
+            console.log(`Delay: ${delay}`)
+            setTimeout(()=>{
+                updateMotorState()
+            }, delay)
+        })
+    }, [feedTimes])
+
+    function handleDateChange(event, selectedDate){
+        setDate(selectedDate)
+        setShowDatePicker(false)
+    }
+
+    function handleTimeChange(event, selectedTime){
+        const currentTime = selectedTime || time;
+        setTime(currentTime);
+        setDate(currentTime)
+        setShowTimePicker(false)
+    }
+    function handleFeedOnSetTime(){
+        const time = new Date(`${date}`).getTime();
+        const delay = time - Date.now();
+        if(delay < 0) {
+            alert("You can not set feeding time on the past :)")
+            return;}
+        updateDoc(deviceRef, {
+            feedTimes: [...feedTimes, time]
+        }).then(getDoc(deviceRef).then(res=> {
+            if(res.data().feedTimes.length < 1) return;
+            setFeedTimes(res.data().feedTimes.filter(times=> times > Date.now()));
+        })).catch(err=> console.log(err?.message))    
+    }
+    
+ 
+
+    return <Modal
+    animationType='slide'
+    transparent={true}
+    visible={dateModalOpen}
+    >
+          <View style={{flex:1, justifyContent: "center", alignItems: "center",}}>
+            <View style={{backgroundColor: "#fff", height: 480, width: "90%", borderRadius: 10, justifyContent: "center", alignItems: "center", gap:20,  elevation: 8}}>
+                <View>
+                {showDatePicker && <DateTimePicker 
+                    mode={'date'}
+                    value={date || new Date()}
+                    onChange={handleDateChange}
+                />}
+                {
+                    showTimePicker && <DateTimePicker 
+                    mode={"time"}
+                    value={date}
+                    onChange={handleTimeChange}
+                />
+                }
+                </View>
+                <Text>{`${date}`}</Text>
+                <TouchableHighlight onPress={()=> setShowDatePicker(true)}>
+                    <Text>Show Date Picker</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={()=> setShowTimePicker(true)}>
+                    <Text>Show Time Picker</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={handleFeedOnSetTime}>
+                    <Text>Set feed time</Text>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={()=> setDateModalOpen(false)}>
+                    <Text>Close</Text>
+                </TouchableHighlight>
+            </View>
+        </View>
+    </Modal>
 }
 
 export default DeviceScreen
